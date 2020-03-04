644c58a6e94bad469a14ea15f823954fab00540b
Khoa Nguyễn <khoa002@gmail.com>
2020-03-03 22:42:00 -0700

    Finish sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (lower, upper) {
+    let sum = 0;
+    if (typeof lower !== 'number' || typeof upper !== 'number') {
+        return 'ERROR';
+    }
+    if (lower < 0 || upper < 0) {
+        return 'ERROR';
+    }
+    if (lower > upper) {
+        let oldLower = lower;
+        lower = upper;
+        upper = oldLower;
+    }
+    for (let i = lower; i <= upper; i++) {
+        sum += i;
+    }
 
+    return sum;
 }
 
 module.exports = sumAll

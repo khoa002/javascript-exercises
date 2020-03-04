9359f20f738ad53b330a0e2250e725d8632d3a19
Khoa Nguyễn <khoa002@gmail.com>
2020-03-03 23:20:29 -0700

    Finish tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
-
+/**
+ * Convert a temperature from Fahrenheit to Celsius
+ * [C] = ([F] - 32) * 5/9
+ * Result is rounded to one decimal place: 00.0
+ */
+const ftoc = function (temperature) {
+  let result = (temperature - 32) * (5 / 9);
+  return parseFloat(result.toFixed(1));
 }
 
-const ctof = function() {
-
+/**
+ * Convert a temperature from Celsius to Fahrenheit
+ * [F] = [C] * ​9/5 + 32
+ * Result is rounded to one decimal place: 00.0
+ */
+const ctof = function (temperature) {
+  let result = temperature * (9 / 5) + 32;
+  return parseFloat(result.toFixed(1));
 }
 
 module.exports = {

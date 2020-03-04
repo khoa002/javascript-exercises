cd88032cae45795e39bd5bee392308bc51878423
Khoa Nguyễn <khoa002@gmail.com>
2020-03-03 22:17:12 -0700

    Finish removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let args = [...arguments];
+    let result = args[0];
+    args.forEach(function (value, index) {
+        if (index == 0) {
+            // if this is the first index, continue with the loop
+            return true;
+        }
+        let indexToRemove = result.indexOf(value);
+        if (indexToRemove > -1) {
+            result.splice(indexToRemove, 1);
+        }
+    });
 
+    return result;
 }
 
 module.exports = removeFromArray

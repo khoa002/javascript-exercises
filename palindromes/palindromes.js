2e616a0c3df41fd63f37bf1514067f8655509358
Khoa Nguyễn <khoa002@gmail.com>
2020-03-11 13:33:47 -0700

    Finish palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
-
+const palindromes = function (string) {
+    let cleaned = string.replace(/[^a-zA-Z]+/g, '').toLowerCase();
+    let halfLen = Math.floor(cleaned.length / 2);
+    for (let i = 0; i < halfLen; i++) {
+        if (cleaned.charAt(i) != cleaned.charAt(cleaned.length - (i + 1))) {
+            return false
+        }
+    }
+    return true;
 }
 
 module.exports = palindromes

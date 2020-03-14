6d0efffd4874d17c8acab97f0f0d174829a628db
Khoa Nguyễn <khoa002@gmail.com>
2020-03-13 18:36:39 -0700

    Finsh getTheTitles exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+    let result = [];
+    for (let o of obj) {
+        result.push(o.title);
+    }
+    return result;
 }
 
 module.exports = getTheTitles;

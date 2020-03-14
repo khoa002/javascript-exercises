e6b256db58f38867fc2117cfe8c1eabb81bd14f0
Khoa Nguyễn <khoa002@gmail.com>
2020-03-13 18:50:43 -0700

    Finish findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-let findTheOldest = function() {
-
+let findTheOldest = function (people) {
+    let oldest = people[0];
+    for (let p of people) {
+        let currentYear = new Date().getFullYear();
+        let cAge = ('yearOfDeath' in oldest ? oldest.yearOfDeath : currentYear) - oldest.yearOfBirth;
+        let pAge = ('yearOfDeath' in p ? p.yearOfDeath : currentYear) - p.yearOfBirth;
+        if (pAge > cAge) {
+            oldest = p;
+        }
+    }
+    return oldest;
 }
 
 module.exports = findTheOldest

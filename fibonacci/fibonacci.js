686b6cbfdf20dd607c42925f32be14d330bcae28
Khoa Nguyễn <khoa002@gmail.com>
2020-03-13 18:29:11 -0700

    Finish fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+// a series of numbers in which each number ( Fibonacci number )
+// is the sum of the two preceding numbers.
+// The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+const fibonacci = function (pos) {
+    pos = parseInt(pos);
+    if (pos < 0) {
+        return 'OOPS';
+    }
+    if (pos < 3) {
+        return 1;
+    }
 
+    let fib = [1, 1];
+    for (let i = 2; i < pos; i++) {
+        fib.push(fib[i - 2] + fib[i - 1]);
+    }
+    return fib.pop();
 }
 
 module.exports = fibonacci
